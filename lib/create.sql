e8908330b963aa5518a1f9acd4bd2f741bb17f88
Maurice Fiorani <mauricef97@gmail.com>
2020-03-24 18:33:39 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+-- (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
