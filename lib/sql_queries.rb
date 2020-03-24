c8455da99e15969dbb289aa500694859301d3e95
Maurice Fiorani <mauricef97@gmail.com>
2020-03-24 17:59:27 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,7 +5,7 @@ end
 # (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
